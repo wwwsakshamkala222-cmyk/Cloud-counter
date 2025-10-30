0afb67851112bea61f5e4d1c2dd3ae333d38597c
Saksham kala <www.sakshamkala222@gmail.com>
2025-10-30 11:42:04 +0530

    cloud

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState('loading...');
+  useEffect(() => {
+    fetch('https://zkd3gloogkyn4u5atq46ko5ov40ndail.lambda-url.eu-north-1.on.aws/')
+      .then(res => res.json())
+      .then(data => setCount(data.count))
+      .catch(() => setCount('error'));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{fontFamily:'Inter, sans-serif',textAlign:'center',marginTop:'100px'}}>
+      <h1>Your Name</h1>
+      <p>B.Tech Student | Cloud Developer</p>
+      <h3>Visitor Count: {count}</h3>
     </div>
   );
 }
